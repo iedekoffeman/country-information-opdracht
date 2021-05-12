dee8ece0ee43d3f9c2527c7eacc611dea75b48c0
iedekoffeman <i.koffeman@gmail.com>
2021-05-12 13:31:31 +0200

    Added assignment 3

--- main.js
+++ main.js
@@ -8,10 +8,13 @@ searchButton.addEventListener('click', async () => {
         "https://restcountries.eu/rest/v2/name/belgium"
 
     );
+    console.log(response.data);
+    //Opdracht2 en 3
     let country = response.data[0].name;
+    let capital = response.data[0].capital;
     let subRegion = response.data[0].subregion;
     let population = response.data[0].population;
     console.log(`${country} is situated in ${subRegion}. It has a population of ${population} people`);
-
+    console.log(`The capital is ${capital}`);
 });
 
